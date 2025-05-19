65c5efb3416a61e3711bb705f01f31db7f6b4318
404Games <162417562+404GamesGithub@users.noreply.github.com>
2025-05-18 20:33:49 -0700

    copy

---
+++ rtb0.doubleverify.com/verify.js
@@ -0,0 +1,10 @@
+
+
+try{__tagObject_callback_1747625511431961({ImpressionID:"00892a67a38d418dad7089c8559065ef", ServerPublicDns:"rtbc-uw1.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "00892a67a38d418dad7089c8559065ef");}catch(e){}
+try{__verify_callback_1747625511431961({
+ResultID:2,
+Passback:"",
+AdWidth:300,
+AdHeight:600});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "00892a67a38d418dad7089c8559065ef");}catch(e){}
